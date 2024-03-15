08e6edef91ed965c4fa32912eb78307843d6a8a3
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-03-15 03:16:40 +0300

    feat(shader): some sdfs added

--- shaders/fragment-shader.glsl
+++ shaders/fragment-shader.glsl
@@ -3,6 +3,9 @@ varying vec2 vUvs;
 uniform vec2 resolution;
 uniform float time;
 
+const int NUM_STEPS = 256;
+const float MAX_DIST = 1000.0;
+
 float inverseLerp(float v, float minValue, float maxValue) {
 	return (v - minValue) / (maxValue - minValue);
 }
@@ -16,18 +19,55 @@ float saturate(float x) {
   	return clamp(x, 0.0, 1.0);
 }
 
-float sphereSDF(vec2 pos, vec2 center, float r) {
-	return distance(pos, center) - r;
+float sdfSphere(vec3 pos, float r) {
+	return length(pos) - r;
 }
 
-float sceneSDF(vec2 pos) {
-	return sphereSDF(pos, vec2(0.0, 0.0), 10.0);
+float sdfBox(vec3 p, vec3 b) {
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float sdfTorus(vec3 p, vec2 t) {
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
+}
+
+float calculateSceneSDF(vec3 pos) {
+	//float dist = sdfSphere(pos - vec3(0.0, 0.0, 5.0), 1.0);
+	//float dist = sdfBox(pos - vec3(0.0, 0.0, 5.0), vec3(1.0));
+	float dist = sdfTorus(pos - vec3(0.0, 0.0, 5.0), vec2(1.0));
+	return dist;
+}
+
+vec3 rayMarch(vec3 cameraOrigin, vec3 cameraDir) {
+	vec3 pos;
+	float dist = 0.0;
+
+	for (int i = 0; i < NUM_STEPS; ++i) {
+		pos = cameraOrigin + cameraDir * dist;
+		float distToScene = calculateSceneSDF(pos);
+
+		if (distToScene < 0.001) {
+			break;
+		}
+
+		dist += distToScene;
+
+		if (dist > MAX_DIST) {
+			return vec3(0.0);
+		}
+	}
+	return vec3(1.0);
 }
 
 void main() {
 	vec2 pixelCoords = (vUvs - 0.5) * resolution;
 
-  	vec3 colour = vec3(0.0, 1.0, 0.0);
+	vec3 rayDir = normalize(vec3(pixelCoords * 2.0 / resolution.y, 1.0));
+	vec3 rayOrigin = vec3(0.0);
+
+  	vec3 colour = rayMarch(rayOrigin, rayDir);
 
   	gl_FragColor = vec4(pow(colour, vec3(1.0 / 2.2)), 1.0);
 }
